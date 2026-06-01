25988b1ca9942862175d6dab8a94228f51f66cad
Aditya Anand <aditya001anand@gmail.com>
2026-06-01 22:08:24 +0530

    Implement data shuffling and MSE calculation
    Added data shuffling and test set evaluation.

--- numpy_linear.py
+++ numpy_linear.py
@@ -1,19 +1,24 @@
 import numpy as np
 import pandas as pd
 
-data = pd.read_csv('data.csv')
-cols = ['LongestShell', 'Diameter', 'Height', 'WholeWeight', 'ShuckedWeight', 'VisceraWeight', 'ShellWeight'] 
-
-x = data[cols].values
-y = data['Rings'].values
-
-split = int(0.8 * len(x))
-x_train, y_train = x[:split], y[:split]
-
-ones = np.ones((len(x_train), 1))
-x_train_b = np.c_[ones, x_train]
-
-w = np.linalg.pinv(x_train_b.T.dot(x_train_b)).dot(x_train_b.T).dot(y_train)
+data=pd.read_csv('data.csv')
+columns=['LongestShell','Diameter','Height','WholeWeight','ShuckedWeight','VisceraWeight','ShellWeight']
+x=data[columns].values
+y=data['Rings'].values
+np.random.seed(42)
+idx=np.random.permutation(len(x))
+x,y=x[idx],y[idx]
+split=int(0.8*len(x))
+x_t,y_t=x[:split],y[:split]
+x_test,y_test=x[split:],y[split:]
+ones=np.ones((len(x_t),1))
+x_t_b=np.c_[ones,x_t]
+w=np.linalg.pinv(x_t_b.T.dot(x_t_b)).dot(x_t_b.T).dot(y_t)
+test_ones = np.ones((len(x_test),1))
+x_test_b=np.c_[test_ones,x_test]
+preds=x_test_b.dot(w)
+mse=np.mean((preds-y_test)**2)
 
 print(f"bias: {w[0]:.4f}")
-print(f"weights: {[round(weight, 4) for weight in w[1:]]}")
+print(f"weights: {[round(weight,4) for weight in w[1:]]}")
+print(f"mse: {mse:.4f}")
